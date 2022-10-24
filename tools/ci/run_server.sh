e84e28ec30794c07b2075137290f568fbe776844
dj-34 <dj-34@bk.ru>
2022-10-24 16:35:02 +0500

    Fix: Error code 134

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -8,7 +8,14 @@ rm config/dbconfig.txt
 cp tools/ci/dbconfig.txt config
 
 # Now run the server and the unit tests
-DreamDaemon paradise.dmb -close -trusted -verbose
+DreamDaemon paradise.dmb -close -trusted -verbose || EXIT_CODE=$?
+
+# We don't care if extools dies
+if [ $EXIT_CODE != 134 ]; then
+   if [ $EXIT_CODE != 0 ]; then
+      exit $EXIT_CODE
+   fi
+fi
 
 # Check if the unit tests actually suceeded
 cat data/clean_run.lk
