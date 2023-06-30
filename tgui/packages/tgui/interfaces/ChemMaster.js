5884c378b6f64162b9dfff7470622724a0c8cf20
Gottfrei <zweisen@gmail.com>
2023-06-30 15:34:30 +0300

    Resizing Icons For TGUI

--- tgui/packages/tgui/interfaces/ChemMaster.js
+++ tgui/packages/tgui/interfaces/ChemMaster.js
@@ -3,7 +3,7 @@ import { useBackend } from "../backend";
 import { Box, Button, Flex, Icon, LabeledList, Section } from "../components";
 import { Window } from "../layouts";
 import { BeakerContents } from './common/BeakerContents';
-import { ComplexModal, modalOpen, modalRegisterBodyOverride } from './common/ComplexModal';
+import { ComplexModal, modalOpen, modalAnswer, modalRegisterBodyOverride } from './common/ComplexModal';
 
 const transferAmounts = [1, 5, 10];
 const bottleStyles = [
@@ -59,6 +59,35 @@ const analyzeModalBodyOverride = (modal, context) => {
         </LabeledList>
       </Box>
     </Section>
+  );
+};
+
+const changePatchStyleModalBodyOverride = (modal, context) => {
+  const { data } = useBackend(context);
+  const result = modal.args.analysis;
+  return (
+    <Flex
+      spacingPrecise="1"
+      wrap="wrap"
+      my="0.5rem"
+      maxHeight="1%">
+      {data.modal.choices.map((c, i) => (
+        <Flex.Item key={i} flex="1 1 auto">
+          <Button
+            selected={(i + 1) === parseInt(data.modal.value, 10)}
+            onClick={() => modalAnswer(context, modal.id, i + 1)}>
+            <div style={
+              "display: inline-block;"
+              + "width: 32px;"
+              + "height: 32px;"
+              + "background: url(bandaid" + (i + 1) + ".png);"
+              + "background-size: 160%;"
+              + "background-position: left -9px bottom -14px;"
+            } />
+          </Button>
+        </Flex.Item>
+      ))}
+    </Flex>
   );
 };
 
@@ -419,3 +448,4 @@ const ChemMasterCustomization = (props, context) => {
 };
 
 modalRegisterBodyOverride('analyze', analyzeModalBodyOverride);
+modalRegisterBodyOverride('change_patch_style', changePatchStyleModalBodyOverride);
