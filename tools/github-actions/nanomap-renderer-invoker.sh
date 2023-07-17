f29010f59c6ba14b89585cfe7f04acce8253d5e5
Biegemot <66844527+Biegemot@users.noreply.github.com>
2023-07-17 16:58:47 +0700

    Update nanomap-renderer-invoker.sh

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -5,10 +5,10 @@ tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files
 tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 "./_maps/map_files/cerestation/cerestation.dmm"
 # Move and rename files so the game understands them
 cd "data/nanomaps"
-mv "cyberiad_nanomap_z1.png" "Cyberiad_nanomap_z1.png"
+mv "utopia_nanomap_z1.png" "Utopia_nanomap_z1.png"
 mv "delta_nanomap_z1.png" "Delta_nanomap_z1.png"
 mv "cerestation_nanomap_z1.png" "Cerestation_nanomap_z1.png"
 cd "../../"
-cp "data/nanomaps/Cyberiad_nanomap_z1.png" "icons/_nanomaps"
+cp "data/nanomaps/Utopia_nanomap_z1.png" "icons/_nanomaps"
 cp "data/nanomaps/Delta_nanomap_z1.png" "icons/_nanomaps"
 cp "data/nanomaps/Cerestation_nanomap_z1.png" "icons/_nanomaps"
