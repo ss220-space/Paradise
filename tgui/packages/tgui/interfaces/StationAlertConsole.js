d4daca59076311393e0604c81346f2ca6580307b
0x0v3rfl0w <140404099+0x0v3rfl0w@users.noreply.github.com>
2023-09-23 18:55:25 +0700

    tgui and alarm into alarms_listend_for list

--- tgui/packages/tgui/interfaces/StationAlertConsole.js
+++ tgui/packages/tgui/interfaces/StationAlertConsole.js
@@ -19,6 +19,7 @@ export const StationAlertConsoleContent = (props, context) => {
   const fire = categories['Fire'] || [];
   const atmos = categories['Atmosphere'] || [];
   const power = categories['Power'] || [];
+  const camera = categories['Camera'] || [];
   return (
     <Fragment>
       <Section title="Fire Alarms">
@@ -57,6 +58,20 @@ export const StationAlertConsoleContent = (props, context) => {
             </li>
           )}
           {power.map(alert => (
+            <li key={alert} className="color-average">
+              {alert}
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <Section title="Camera Alarms">
+        <ul>
+          {camera.length === 0 && (
+            <li className="color-good">
+              Systems Nominal
+            </li>
+          )}
+          {camera.map(alert => (
             <li key={alert} className="color-average">
               {alert}
             </li>
