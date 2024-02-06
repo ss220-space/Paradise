5f475a8fd612522e8f67b2fd9b0ebc828bc13d08
ROdenFL <144662735+ROdenFL@users.noreply.github.com>
2024-02-06 20:49:26 +0300

    ы

--- SQL/updates/31-32.sql
+++ SQL/updates/31-32.sql
@@ -1,5 +1,5 @@
 # Add exploit records
 ALTER TABLE `characters`
-	ADD COLUMN `exploit_record` longtext COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT  ` ` AFTER `gen_record`;
+	ADD COLUMN `exploit_record` longtext COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ` ` AFTER `gen_record`;
 # Adds support for persistent ghost darkness
 ALTER TABLE `player` ADD COLUMN `ghost_darkness_level` tinyint(1) UNSIGNED NOT NULL DEFAULT '255'
