60351dd001042dd0d8d26573e0a296ad43c41ab9
Dimach <dimach2142@gmail.com>
2024-04-27 10:48:11 +0000

    tgui components

--- tgui/packages/tgui/components/Icon.js
+++ tgui/packages/tgui/components/Icon.js
@@ -9,10 +9,19 @@ import { Box } from './Box';
 
 const FA_OUTLINE_REGEX = /-o$/;
 
-export const Icon = props => {
-  const { name, size, spin, className, style = {}, rotation, ...rest } = props;
+export const Icon = (props) => {
+  const {
+    name,
+    size,
+    spin,
+    className,
+    style = {},
+    rotation,
+    inverse,
+    ...rest
+  } = props;
   if (size) {
-    style['font-size'] = (size * 100) + '%';
+    style['font-size'] = size * 100 + '%';
   }
   if (typeof rotation === 'number') {
     style['transform'] = `rotate(${rotation}deg)`;
@@ -23,14 +32,32 @@ export const Icon = props => {
     <Box
       as="i"
       className={classes([
+        'Icon',
         className,
         faRegular ? 'far' : 'fas',
         'fa-' + faName,
         spin && 'fa-spin',
       ])}
       style={style}
-      {...rest} />
+      {...rest}
+    />
   );
 };
 
 Icon.defaultHooks = pureComponentHooks;
+
+export const IconStack = (props) => {
+  const { className, style = {}, children, ...rest } = props;
+  return (
+    <Box
+      as="span"
+      class={classes(['IconStack', className])}
+      style={style}
+      {...rest}
+    >
+      {children}
+    </Box>
+  );
+};
+
+Icon.Stack = IconStack;
