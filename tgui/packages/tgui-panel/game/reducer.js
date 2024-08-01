0a12d67dca6ad37009c4bfe216f459d9fe0e5d35
Aziz Chynaliev <hello@aziz.space>
2024-08-01 23:52:20 +0600

    feat: increase auto reconnection delay and add random delay

--- tgui/packages/tgui-panel/game/reducer.js
+++ tgui/packages/tgui-panel/game/reducer.js
@@ -9,6 +9,12 @@ import { connectionRestored } from './actions';
 import { roundRestarted } from './actions';
 import { AUTO_RECONNECT_AFTER } from './constants';
 
+const getRandomIntInclusive = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
+
 const initialState = {
   // TODO: This is where round info should be.
   roundId: null,
@@ -24,7 +30,11 @@ export const gameReducer = (state = initialState, action) => {
     return {
       ...state,
       roundRestartedAt: meta.now,
-      autoReconnectAfter: meta.now + AUTO_RECONNECT_AFTER,
+      autoReconnectAfter:
+        // Add a random amount of time to the auto reconnect time
+        // to avoid all clients reconnecting at the same time
+        // from 0 to 15 seconds with 100ms step
+        meta.now + AUTO_RECONNECT_AFTER + getRandomIntInclusive(0, 150) * 100,
     };
   }
   if (type === connectionLost.type) {
