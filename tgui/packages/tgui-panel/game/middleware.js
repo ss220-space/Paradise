431efd7a385710f4db2af52b959a971baebd7e5e
Aziz Chynaliev <hello@aziz.space>
2024-08-01 19:15:47 +0600

    server: auto reconnect on round restart after 10s (#5633)

--- tgui/packages/tgui-panel/game/middleware.js
+++ tgui/packages/tgui-panel/game/middleware.js
@@ -5,9 +5,14 @@
  */
 
 import { pingSoft, pingSuccess } from '../ping/actions';
-import { connectionLost, connectionRestored, roundRestarted } from './actions';
+import {
+  connectionLost,
+  connectionRestored,
+  roundRestarted,
+  autoReconnect,
+} from './actions';
 import { selectGame } from './selectors';
-import { CONNECTION_LOST_AFTER } from './constants';
+import { CONNECTION_LOST_AFTER, AUTO_RECONNECT_AFTER } from './constants';
 
 const withTimestamp = (action) => ({
   ...action,
@@ -27,11 +32,16 @@ export const gameMiddleware = (store) => {
     const game = selectGame(state);
     const pingsAreFailing =
       lastPingedAt && Date.now() >= lastPingedAt + CONNECTION_LOST_AFTER;
+    const shouldAutoReconnect =
+      game.autoReconnectAfter && Date.now() >= game.autoReconnectAfter;
     if (!game.connectionLostAt && pingsAreFailing) {
       store.dispatch(withTimestamp(connectionLost()));
     }
     if (game.connectionLostAt && !pingsAreFailing) {
       store.dispatch(withTimestamp(connectionRestored()));
+    }
+    if (shouldAutoReconnect) {
+      store.dispatch(autoReconnect());
     }
   }, 1000);
   return (next) => (action) => {
@@ -43,6 +53,10 @@ export const gameMiddleware = (store) => {
     }
     if (type === roundRestarted.type) {
       return next(withTimestamp(action));
+    }
+    if (type === autoReconnect.type) {
+      Byond.command('.reconnect');
+      return;
     }
 
     return next(action);
