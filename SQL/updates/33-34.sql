034d9efd32aa95dc1e1cc3b078767342ea2daa03
ROdenFL <144662735+ROdenFL@users.noreply.github.com>
2024-08-18 21:49:48 +0300

    unicode

--- SQL/updates/33-34.sql
+++ SQL/updates/33-34.sql
@@ -2,12 +2,12 @@
 # Adds a new table for instance data caching, and server_id fields on other tables
 
 CREATE TABLE `instance_data_cache` (
-	`server_id` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`key_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`key_value` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`server_id` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`key_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`key_value` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
 	`last_updated` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
 	PRIMARY KEY (`server_id`, `key_name`) USING HASH
-) COLLATE='utf8mb4_general_ci' ENGINE=MEMORY;
+) COLLATE='utf8mb4_unicode_ci' ENGINE=MEMORY;
 
 ALTER TABLE `ban`
 	ADD COLUMN `server_id` VARCHAR(50) NULL DEFAULT NULL AFTER `serverip`;
@@ -26,7 +26,7 @@ ALTER TABLE `legacy_population`
 
 # Notes already has a column for this
 ALTER TABLE `notes`
-	CHANGE COLUMN `server` `server` VARCHAR(50) NULL COLLATE 'utf8mb4_general_ci' AFTER `edits`;
+	CHANGE COLUMN `server` `server` VARCHAR(50) NULL COLLATE 'utf8mb4_unicode_ci' AFTER `edits`;
 
 ALTER TABLE `round`
 	ADD COLUMN `server_id` VARCHAR(50) NULL DEFAULT NULL AFTER `station_name`;
