49f17196cae706e4b78c7286a3fe9b17f93fd5f8
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2025-04-17 17:50:17 +0500

    common ready

--- tgui/packages/common/types.ts
+++ tgui/packages/common/types.ts
@@ -1,6 +1,12 @@
 /**
  * Returns the arguments of a function F as an array.
  */
-// prettier-ignore
-export type ArgumentsOf<F extends Function>
-  = F extends (...args: infer A) => unknown ? A : never;
+export type ArgumentsOf<F extends Fn> = F extends (...args: infer A) => unknown
+  ? A
+  : never;
+
+/**
+ * A function. Use this instead of `Function` to avoid issues with
+ * type inference.
+ */
+export type Fn = (...args: any[]) => void;
