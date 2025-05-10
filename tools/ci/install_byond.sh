9209e21ac3f846d15e0d1da74f20dd7ec63b2696
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2025-05-10 17:39:17 +0500

    test 4

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -23,8 +23,9 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  
+
   if ! curl -L --fail --retry 3 \
+  	-A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" \
     "https://www.byond.com/download/build/${TARGET_MAJOR}/${TARGET_MAJOR}.${TARGET_MINOR}_byond_linux.zip" \
     -o byond.zip; then
     echo "Failed to download BYOND!"
