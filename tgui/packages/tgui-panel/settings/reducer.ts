a02e4acf74dfdbff57c0d64839b2471fbf9c73f1
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2025-05-20 21:13:20 +0500

    dark theme by default + color bugfix

--- tgui/packages/tgui-panel/settings/reducer.ts
+++ tgui/packages/tgui-panel/settings/reducer.ts
@@ -26,7 +26,7 @@ const initialState = {
   fontSize: 13,
   fontFamily: FONTS[0],
   lineHeight: 1.2,
-  theme: 'light',
+  theme: 'dark',
   adminMusicVolume: 0.5,
   // Keep these two state vars for compatibility with other servers
   highlightText: '',
