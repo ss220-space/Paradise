6c09d56e34b96d45858e12ac530a33172ddda86b
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2025-07-03 09:58:13 +0500

    Update api.rs

--- rust/src/subsystem/sun/api.rs
+++ rust/src/subsystem/sun/api.rs
@@ -45,3 +45,8 @@ pub fn get_solars_length() -> eyre::Result<ByondValue> {
 pub fn test_test() -> eyre::Result<ByondValue> {
     SUN.with(|sun| Ok(ByondValue::from(sun.solars.borrow().len() as f32)))
 }
+
+#[byondapi::bind]
+pub fn test_test2() -> eyre::Result<ByondValue> {
+    SUN.with(|sun| Ok(ByondValue::from(sun.solars.borrow().len() as f32)))
+}
