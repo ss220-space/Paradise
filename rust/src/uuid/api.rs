566b213de59d7802c88f589ffc2e735030446d31
Antoonij <pisclov123pis2@gmail.com>
2025-07-04 11:36:12 +0200

    improve

--- rust/src/uuid/api.rs
+++ rust/src/uuid/api.rs
@@ -18,10 +18,9 @@ fn get_by_uuid(key: ByondValue) -> eyre::Result<ByondValue> {
     let key_str = key.get_string()?;
     let uuid = Uuid::parse_str(&key_str)?;
 
-    UUID_STORAGE.with(|storage| {
-        let maybe_val = storage.borrow().get(&uuid).copied();
-        Ok(maybe_val.unwrap_or_else(ByondValue::null))
-    })
+    let val = UUID_STORAGE.with(|storage| storage.borrow().get(&uuid).copied());
+
+    Ok(val.unwrap_or_else(ByondValue::null))
 }
 
 #[byondapi::bind]
@@ -29,9 +28,7 @@ fn untick_by_uuid(key: ByondValue) -> eyre::Result<ByondValue> {
     let key_str = key.get_string()?;
     let uuid = Uuid::parse_str(&key_str)?;
 
-    UUID_STORAGE.with(|storage| {
-        storage.borrow_mut().remove(&uuid);
-    });
+    let val = UUID_STORAGE.with(|storage| storage.borrow_mut().remove(&uuid));
 
-    Ok(ByondValue::null())
+    Ok(val.unwrap_or_else(ByondValue::null))
 }
