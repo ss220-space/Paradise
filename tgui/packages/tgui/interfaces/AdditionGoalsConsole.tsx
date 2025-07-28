54a803d37848237ce8b59b1938c6eca2d7b14c71
Vladimir Konstantinov <anbu23477@gmail.com>
2025-07-28 13:58:16 +0900

    tgui prettier забыл снова

--- tgui/packages/tgui/interfaces/AdditionGoalsConsole.tsx
+++ tgui/packages/tgui/interfaces/AdditionGoalsConsole.tsx
@@ -18,7 +18,6 @@ import { classes } from 'common/react';
 import { Window } from '../layouts';
 import { ComplexModal } from './common/ComplexModal';
 
-
 type AdditionGoalsConsoleData = {
   state: number;
   shuttle_loc: string;
@@ -36,12 +35,16 @@ export const AdditionGoalsConsole = (props: unknown) => {
   const { act, data } = useBackend<AdditionGoalsConsoleData>();
 
   let contentBlock: ReactNode;
-  if(data.state === 10) {
-    contentBlock = <AvailableAdditionGoalsListBlock/>
-  } else if(data.state === 11 || data.state === 20 || data.state === 21) {
-    contentBlock = <CurrentAdditionGoalBlock/>
+  if (data.state === 10) {
+    contentBlock = <AvailableAdditionGoalsListBlock />;
+  } else if (data.state === 11 || data.state === 20 || data.state === 21) {
+    contentBlock = <CurrentAdditionGoalBlock />;
   } else {
-    contentBlock = <Box><h3>Нет данных!</h3></Box>
+    contentBlock = (
+      <Box>
+        <h3>Нет данных!</h3>
+      </Box>
+    );
   }
   return (
     <Window width={600} height={800}>
@@ -49,32 +52,29 @@ export const AdditionGoalsConsole = (props: unknown) => {
         <ComplexModal />
         <Stack fill vertical>
           <Stack.Item>
-            <AdditionGoalsStateBlock/>
+            <AdditionGoalsStateBlock />
           </Stack.Item>
-          <Stack.Item grow>
-            {contentBlock}
-          </Stack.Item>
+          <Stack.Item grow>{contentBlock}</Stack.Item>
         </Stack>
       </Window.Content>
     </Window>
   );
 };
 
-
 const getStateText = (s) => {
-  if(s === 10){
-    return "Нет текущей цели."
+  if (s === 10) {
+    return 'Нет текущей цели.';
   }
-  if(s === 11){
-    return "Шаттл в пути."
+  if (s === 11) {
+    return 'Шаттл в пути.';
   }
-  if(s === 20){
-    return "Цель в процессе выполнения."
+  if (s === 20) {
+    return 'Цель в процессе выполнения.';
   }
-  if(s === 21){
-    return "Завершение цели."
+  if (s === 21) {
+    return 'Завершение цели.';
   }
-  return "Дополнительные цели смены недоступны."
+  return 'Дополнительные цели смены недоступны.';
 };
 
 const AdditionGoalsStateBlock = (props: unknown) => {
@@ -86,37 +86,29 @@ const AdditionGoalsStateBlock = (props: unknown) => {
           <LabeledList.Item label="Состояние">
             {getStateText(data.state)}
           </LabeledList.Item>
-          <LabeledList.Item label="Шаттл">
-            {data.shuttle_loc} <Button
-                icon="rocket"
-                onClick={() => act('call_shuttle')}
-              >
-                Вызвать
-              </Button>
-          </LabeledList.Item>
+          <LabeledList.Item label="Шаттл">{data.shuttle_loc}</LabeledList.Item>
         </LabeledList>
       </Box>
     </Section>
   );
 };
 
-
-
 const AvailableAdditionGoalsListBlock = (props: unknown) => {
   const { act, data } = useBackend<AdditionGoalsConsoleData>();
   const { available_goals } = data;
 
   return (
-    <Section title="Доступные дополнительные цели"
-    buttons={
-      <Button
-        icon="refresh"
-        color={data.refresh_available ? 'green' : 'grey'}
-        onClick={() => act('refresh_available_goals')}
-      >
-        Обновиь список
-      </Button>
-    }
+    <Section
+      title="Доступные дополнительные цели"
+      buttons={
+        <Button
+          icon="refresh"
+          color={data.refresh_available ? 'green' : 'grey'}
+          onClick={() => act('refresh_available_goals')}
+        >
+          Обновиь список
+        </Button>
+      }
     >
       <Box>
         <LabeledList>
@@ -124,7 +116,7 @@ const AvailableAdditionGoalsListBlock = (props: unknown) => {
             <LabeledList.Item label={goal.name} key={goal.name}>
               <Button
                 icon="plus"
-                color='green'
+                color="green"
                 align="center"
                 onClick={() => act('accept_goal', { goal: goal.id })}
               >
@@ -138,7 +130,6 @@ const AvailableAdditionGoalsListBlock = (props: unknown) => {
   );
 };
 
-
 const CurrentAdditionGoalBlock = (props: unknown) => {
   const { act, data } = useBackend<AdditionGoalsConsoleData>();
   const { current_goal } = data;
@@ -147,15 +138,19 @@ const CurrentAdditionGoalBlock = (props: unknown) => {
     <Section title="Текущая дополнительная цель смены">
       <Stack fill vertical>
         <Stack.Item>
-          <Box><b>Идентификатор:</b> {current_goal.id}</Box>
+          <Box>
+            <b>Идентификатор:</b> {current_goal.id}
+          </Box>
         </Stack.Item>
         <Stack.Item>
-          <Box><b>Название:</b> {current_goal.name}</Box>
+          <Box>
+            <b>Название:</b> {current_goal.name}
+          </Box>
         </Stack.Item>
         <Stack.Item>
           <Button
             icon="check"
-            color={data.state === 20 ? 'green' : 'grey' }
+            color={data.state === 20 ? 'green' : 'grey'}
             width="180px"
             align="center"
             fontSize="12px"
@@ -167,4 +162,4 @@ const CurrentAdditionGoalBlock = (props: unknown) => {
       </Stack>
     </Section>
   );
-}
+};
