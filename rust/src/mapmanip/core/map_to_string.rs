2c9ac8bb1d1ec8fa48367f59c70b173049030c64
Antoonij <pisclov123pis2@gmail.com>
2025-08-17 09:15:27 +0200

    more things

--- rust/src/mapmanip/core/map_to_string.rs
+++ rust/src/mapmanip/core/map_to_string.rs
@@ -1,7 +1,7 @@
 /// Turns spacemandmm map object to string.
 pub fn map_to_string(map: &dmmtools::dmm::Map) -> eyre::Result<String> {
-    let mut v = vec![];
-    map.to_writer(&mut v)?;
-    let s = String::from_utf8(v)?;
-    Ok(s)
+    let mut vec = vec![];
+    map.to_writer(&mut vec)?;
+    let string = String::from_utf8(vec)?;
+    Ok(string)
 }
