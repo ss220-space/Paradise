ec274317f0b0fca308a05d3cf44a81671b7b3523
Antoonij <pisclov123pis2@gmail.com>
2025-08-31 17:06:52 +0200

    1

--- rust/src/mapmanip/mod.rs
+++ rust/src/mapmanip/mod.rs
@@ -149,8 +149,12 @@ fn mapmanip_submap_extract_insert(
         submap_size_z.try_into().wrap_err("invalid submap_size_z")?,
     );
 
-    // get the submaps map
+    #[cfg(not(test))]
     let submaps_dmm: std::path::PathBuf = submaps_dmm.into();
+
+    #[cfg(test)]
+    let submaps_dmm = std::path::Path::new("../").join(submaps_dmm);
+
     let submaps_map = GridMap::from_file(&submaps_dmm)
         .wrap_err(format!("can't read and parse submap dmm: {submaps_dmm:?}"))?;
 
