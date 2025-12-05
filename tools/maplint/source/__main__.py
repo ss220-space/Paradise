f80e3aa648dc1d10a3432587c8b1c69d29e0ed09
littleboobs <arsboy30@gmail.com>
2025-12-05 04:53:25 +0300

    extra UNICODE encoding
    в yml нет юникода

--- tools/maplint/source/__main__.py
+++ tools/maplint/source/__main__.py
@@ -66,7 +66,7 @@ def main(args):
 
     for lint_filename in lint_filenames:
         try:
-            lints[lint_filename] = lint.Lint(yaml.safe_load(lint_filename.read_text(encoding='utf-8')))
+            lints[lint_filename] = lint.Lint(yaml.safe_load(lint_filename.read_text()))
         except MaplintError as error:
             print_maplint_error(error, github_error_style)
             any_failed = True
