b9beb10610335b8cd258f11d054884994ba4b167
PlayerUnknown14 <gordovvladislav@yandex.ru>
2026-01-09 22:55:34 +0700

    Обновление грепа (проклятая регулярка)

--- tools/ci/check_grep2.py
+++ tools/ci/check_grep2.py
@@ -166,7 +166,7 @@ def check_global_list_empty(idx, line):
         return [(idx + 1, "Found a GLOBAL_LIST_INIT(_, list()), please use GLOBAL_LIST_EMPTY(_) instead.")]
 
 # makes sure arguments contained within "ui = new" are valid
-TGUI_UI_NEW = re.compile(r"ui = new\(((?:(?!,\s*).)+,\s*){1,3}(?:(?!,\s*).)+\)")
+TGUI_UI_NEW = re.compile(r'ui = new\((?:[^,\(\)]+(?:\([^)]*\))?|"[^"]*")(?:\s*,\s*(?:[^,\(\)]+(?:\([^)]*\))?|"[^"]*")){1,3}\)')
 def check_tgui_ui_new_argument(idx, line):
     if "\tui = new" in line and not TGUI_UI_NEW.search(line):
         return [(idx + 1, "Invalid argument within constructor, please make sure window sizing is in corresponding TypeScript file.")]
@@ -354,11 +354,10 @@ def check_localization_macro_usage(idx, line):
         if 'UNLINT' not in line:
             return [(idx + 1, "Do not use this proc directly. Use the ready-made macros in code/__HELPERS/localization.dm")]
 
-IGNORE_ATOM_LOCALIZATION_FILE = "atom_localization.dm"
-CAPITALIZED_DECLENT_RU = re.compile(r'capitalize\((?:\w+\.)?declent_ru\(\w+\)\)')
+CAPITALIZED_DECLENT_RU = re.compile(r'capitalize\(\w+\.declent_ru\(\w+\)\)')
 def check_capitalized_declent_ru_usage(idx, line):
     if CAPITALIZED_DECLENT_RU.search(line):
-        return [(idx + 1, "Do not use `capitalize(declent_ru)` construction directly. Use the ready-made macros in code/game/atom/atom_localization.dm")]
+        return [(idx + 1, "Do not use `capitalize(declent_ru)` construction directly. Use the ready-made macros in code/__HELPERS/localization.dm")]
 
 CODE_CHECKS = [
     check_space_indentation,
@@ -507,7 +506,6 @@ def lint_file(code_filepath: str) -> list[Failure]:
             extra_checks.append(check_fast_load_define)
         if filename != IGNORE_LOCALIZATION_HELPERS_FILE:
             extra_checks.append(check_localization_macro_usage)
-        if filename != IGNORE_ATOM_LOCALIZATION_FILE:
             extra_checks.append(check_capitalized_declent_ru_usage)
 
         for idx, line in enumerate(lines):
