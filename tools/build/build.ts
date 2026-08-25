a814b7a342ba6e2a575fb04bdf2e4c7ba7fb3b34
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2026-08-25 23:49:43 +0500

    Фиксы

--- tools/build/build.ts
+++ tools/build/build.ts
@@ -22,9 +22,7 @@ export const DME_NAME = 'paradise';
 
 Juke.chdir('../..', import.meta.url);
 
-const dependencies: Record<string, string> = await Bun.file(
-  '_build_dependencies.sh',
-)
+const dependencies: Record<string, string> = await Bun.file('_build_dependencies.sh')
   .text()
   .then(formatDeps)
   .catch((err) => {
@@ -46,13 +44,13 @@ function getCutterPath() {
 
 const cutter_path = getCutterPath();
 
-const define_params_file = 'data/last_define_params.json';
+const define_params_file = 'data/last_define_params.json'
 
 // Have compilation defines changed since last build?
 async function defineParametersChanged(defines: string[]): Promise<boolean> {
   const defines_string = JSON.stringify(defines);
   const params_file = Bun.file(define_params_file);
-  if (!(await params_file.exists())) {
+  if(!await params_file.exists()) {
     await params_file.write(defines_string);
     return true;
   }
@@ -175,7 +173,11 @@ export const DmMapsIncludeTarget = new Juke.Target({
 });
 
 export const BehaviorTreeCompilerTarget = new Juke.Target({
-  inputs: ['code/**/*.bt.json', 'code/__DEFINES/**/*.dm', 'tools/build_bt.py'],
+  inputs: [
+    'code/**/*.bt.json',
+    'code/__DEFINES/**/*.dm',
+    'tools/build_bt.py',
+  ],
   outputs: () => {
     return Juke.glob('code/**/*.bt.json').map((file) => {
       const rel = file.replace(/\.bt\.json$/, '');
@@ -197,9 +199,8 @@ export const DmTarget = new Juke.Target({
     SkipIconCutter,
   ],
   dependsOn: ({ get }) => [
-    get(DefineParameter).includes('ALL_TEMPLATES') && DmMapsIncludeTarget,
-    !get(SkipIconCutter) && IconCutterTarget,
-    //BehaviorTreeCompilerTarget,
+    get(DefineParameter).includes('ALL_MAPS') && DmMapsIncludeTarget,
+    !get(SkipIconCutter) && IconCutterTarget
   ],
   inputs: [
     '_maps/map_files/generic/**',
@@ -214,10 +215,7 @@ export const DmTarget = new Juke.Target({
     NamedVersionFile,
   ],
   outputs: async ({ get }) => {
-    if (
-      get(DmVersionParameter) ||
-      (await defineParametersChanged(get(DefineParameter)))
-    ) {
+    if (get(DmVersionParameter) || await defineParametersChanged(get(DefineParameter))) {
       // Always rebuild when a dm version is provided or CLI defines have changed from last run
       return [];
     }
