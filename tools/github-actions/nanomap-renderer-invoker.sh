09b40333f81219de1d19b4f7250983801b92b82b
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2026-08-27 00:56:10 +0500

    Фикс генерации наномап 2

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Generate maps
-tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 --disable icon-smoothing "./_maps/map_files/cyberiad/cyberiad.dmm"
-tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 --disable icon-smoothing "./_maps/map_files/Delta/delta.dmm"
-tools/github-actions/nanomap-renderer minimap -w 2040 -h 2040 --disable icon-smoothing "./_maps/map_files/nova/nova.dmm"
+~/dmm-tools minimap -w 2040 -h 2040 --disable icon-smoothing "./_maps/map_files/cyberiad/cyberiad.dmm"
+~/dmm-tools minimap -w 2040 -h 2040 --disable icon-smoothing "./_maps/map_files/Delta/delta.dmm"
+~/dmm-tools minimap -w 2040 -h 2040 --disable icon-smoothing "./_maps/map_files/nova/nova.dmm"
+
 # Move and rename files so the game understands them
 cd "data/nanomaps"
 mv "cyberiad_nanomap_z1.png" "Cyberiad_nanomap_z1.png"
